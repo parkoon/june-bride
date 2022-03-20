74959e153379755f7ef586c46513d33c774a1181
parkoon <devparkoon@gmail.com>
2022-03-20 10:39:08 +0900

    component 의 common 폴더명 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
 import type { NextPage } from 'next'
+import { useEffect } from 'react'
+
+import EmptyBox from '@components/common/FakeBox'
 
 const Home: NextPage = () => {
-  return <div>Hello world</div>
+  useEffect(() => {
+    const handleScroll = () => {
+      console.log(window.scrollY)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+  return (
+    <div>
+      Hello world
+      <EmptyBox opacity={0.1} />
+      <EmptyBox opacity={0.2} />
+      <EmptyBox opacity={0.3} />
+    </div>
+  )
 }
 
 export default Home
