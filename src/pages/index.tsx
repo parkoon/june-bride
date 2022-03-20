7fe9a123c21543c602c1bc68e1a26e4753563a58
parkoon <devparkoon@gmail.com>
2022-03-20 18:32:30 +0900

    app test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 
 import EmptyBox from '@components/common/FakeBox'
 import Layout from '@components/common/Layout'
+import Envelope from '@components/sections/Envelope'
 import Landing from '@components/sections/Landing'
 
 import useScrollAnimationEffect from '@hooks/useScrollAnimationEffect'
@@ -12,12 +13,14 @@ const Home: NextPage = () => {
   return (
     <Layout>
       <Landing />
-      <EmptyBox opacity={0.1} id={EMPTY_SCENE_ID}>
+
+      <Envelope />
+      {/* <EmptyBox opacity={0.1} id={EMPTY_SCENE_ID}>
         <button style={{ width: 200, height: 200 }} type="button">
           클릭 테스트
         </button>
       </EmptyBox>
-      <EmptyBox opacity={0.3} />
+      <EmptyBox opacity={0.3} /> */}
     </Layout>
   )
 }
