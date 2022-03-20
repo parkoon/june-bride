74fc27fce73a20f8d1062b8559822514c251188f
parkoon <devparkoon@gmail.com>
2022-03-20 16:06:57 +0900

    update layout & fake box

--- src/components/common/FakeBox.tsx
+++ src/components/common/FakeBox.tsx
@@ -1,7 +1,8 @@
 import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 
-const EmptyBox = styled.div<{ opacity: number }>`
+const EmptyBox = styled.section<{ opacity: number }>`
+  position: relative;
   height: 100vh;
   ${({ opacity }) => css`
     background: rgba(0, 0, 0, ${opacity});
--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,11 +1,8 @@
 import styled from '@emotion/styled'
 
 const Wrapper = styled.main`
-  max-width: 768px;
   width: 100%;
   background: rgba(0, 34, 0, 0.3);
-
-  margin: 0 auto;
 `
 
 type Props = {
