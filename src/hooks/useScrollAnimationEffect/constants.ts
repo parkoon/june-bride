91097a0fb2b9c3a65703f2dc259cc10e3199caea
parkoon <devparkoon@gmail.com>
2022-03-20 18:32:12 +0900

    add envelop scene

--- src/hooks/useScrollAnimationEffect/constants.ts
+++ src/hooks/useScrollAnimationEffect/constants.ts
@@ -1,12 +1,20 @@
 export const LANDING_SCENE_ID = 'landing'
+export const ENVELOPE_SCENE_ID = 'envelope'
 export const EMPTY_SCENE_ID = 'empty'
 
-export type Scene = typeof LANDING_SCENE_ID | typeof EMPTY_SCENE_ID
+export type Scene =
+  | typeof LANDING_SCENE_ID
+  | typeof EMPTY_SCENE_ID
+  | typeof ENVELOPE_SCENE_ID
 
 export const SCENE = [
   {
     id: LANDING_SCENE_ID,
     long: 5,
+  },
+  {
+    id: ENVELOPE_SCENE_ID,
+    long: 6,
   },
   {
     id: EMPTY_SCENE_ID,
