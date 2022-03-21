a55f46976fe43d8519da68d295e51b4f739339b2
parkoon <devparkoon@gmail.com>
2022-03-21 20:24:39 +0900

    GUIDANCE_SCENE_ID

--- src/hooks/useScrollAnimationEffect/constants.ts
+++ src/hooks/useScrollAnimationEffect/constants.ts
@@ -1,23 +1,29 @@
 export const LANDING_SCENE_ID = 'landing'
 export const ENVELOPE_SCENE_ID = 'envelope'
+export const GUIDANCE_SCENE_ID = 'guidance'
 export const EMPTY_SCENE_ID = 'empty'
 
 export type Scene =
   | typeof LANDING_SCENE_ID
   | typeof EMPTY_SCENE_ID
   | typeof ENVELOPE_SCENE_ID
+  | typeof GUIDANCE_SCENE_ID
 
 export const SCENE = [
-  {
-    id: LANDING_SCENE_ID,
-    long: 5,
-  },
-  {
-    id: ENVELOPE_SCENE_ID,
-    long: 6,
-  },
+  // {
+  //   id: LANDING_SCENE_ID,
+  //   long: 5,
+  // },
+  // {
+  //   id: ENVELOPE_SCENE_ID,
+  //   long: 6,
+  // },
   {
     id: EMPTY_SCENE_ID,
     long: 1,
+  },
+  {
+    id: GUIDANCE_SCENE_ID,
+    long: 6,
   },
 ]
