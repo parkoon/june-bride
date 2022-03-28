7f73bc3b0901dc3eff9cc3e421bcdae8e0f31e32
parkoon <devparkoon@gmail.com>
2022-03-28 16:04:34 +0900

    프로필 추가

--- src/components/sections/Guidance/GuidanceProfile.tsx
+++ src/components/sections/Guidance/GuidanceProfile.tsx
@@ -1,12 +1,50 @@
 import styled from '@emotion/styled'
+import Image from 'next/image'
 import React from 'react'
 
+import AccountCopyButton from '@components/Button/AccountCopyButton'
+import CallButton from '@components/Button/CallButton'
+import MessageButton from '@components/Button/MessageButton'
 import SlidingTab from '@components/common/SlidingTab'
 
 import GuidanceArticle from './GuidanceArticle'
 
-const Content = styled.div``
+const TabWrapper = styled.div`
+  position: absolute;
 
+  left: 12px;
+  top: 12px;
+`
+
+const ImageWrapper = styled.div`
+  position: relative;
+
+  width: 100%;
+  height: 50vh;
+`
+const UserInfo = styled.div`
+  padding: 20px 12px;
+`
+const UserName = styled.h3`
+  font-size: 1.8rem;
+  font-weight: bold;
+  letter-spacing: 1px;
+`
+const Job = styled.span`
+  color: #7f8c8d;
+  font-size: 1.1rem;
+`
+const Status = styled.span``
+
+const Actions = styled.div`
+  margin-top: 24px;
+`
+
+const ActionTitle = styled.span`
+  color: #7f8c8d;
+  font-size: 0.9rem;
+  padding: 12px;
+`
 function GuidanceProfile() {
   return (
     <GuidanceArticle
@@ -16,14 +54,31 @@ function GuidanceProfile() {
         title: 'lorem',
         description: 'Lorem ipsum dolor \n ipsum sit.',
       }}
-      body={{
-        title: 'lorem',
-        description: 'Lorem ipsum dolor \n ipsum sit.',
-      }}
     >
-      <Content>
-        <SlidingTab items={['신랑', '신부']} />
-      </Content>
+      <ImageWrapper>
+        <Image
+          onClick={() => alert('ok')}
+          src={`/images/gallery/1.jpg`}
+          layout="fill"
+          alt="photo"
+          objectFit="cover"
+        />
+
+        <TabWrapper>
+          <SlidingTab items={['신랑', '신부']} />
+        </TabWrapper>
+      </ImageWrapper>
+
+      {/* <UserInfo>
+        <UserName>박 종혁</UserName>
+        <Job>Frontend Engineer</Job>
+      </UserInfo> */}
+      <Actions>
+        <ActionTitle>신랑에게 연락하기</ActionTitle>
+        <MessageButton />
+        <CallButton />
+        <AccountCopyButton />
+      </Actions>
     </GuidanceArticle>
   )
 }
