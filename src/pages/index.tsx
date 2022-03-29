2809063a67730b7a9a2ee56d0c5a8ca345f0896c
parkoon <devparkoon@gmail.com>
2022-03-29 12:36:09 +0900

    build test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from 'next'
 import { useEffect } from 'react'
 
-import EmptyBox from '@components/common/FakeBox'
 import Layout from '@components/common/Layout'
 import Envelope from '@components/sections/Envelope'
-import EnvelopeIcon from '@components/sections/Envelope/EnvelopeIcon'
 import FAQ from '@components/sections/FAQ'
 import Guidance from '@components/sections/Guidance'
 import Landing from '@components/sections/Landing'
@@ -21,30 +19,10 @@ const Home: NextPage = () => {
   }, [])
   return (
     <Layout>
-      {/* <Guidance /> */}
       <Landing />
-      <Envelope />
+      {/* <Envelope />
       <Guidance />
-      <FAQ />
-      {/* <Guidance />
-
       <FAQ /> */}
-      {/* <Landing />
-
-      <Envelope /> */}
-
-      {/* <EmptyBox opacity={0.1} /> */}
-      {/* <EmptyBox opacity={0.3} /> */}
-      {/* <EmptyBox opacity={0.1} id={EMPTY_SCENE_ID} /> */}
-
-      {/* <Card /> */}
-      {/* <EmptyBox opacity={0.3} /> */}
-      {/* <EmptyBox opacity={0.1} id={EMPTY_SCENE_ID}>
-        <button style={{ width: 200, height: 200 }} type="button">
-          클릭 테스트
-        </button>
-      </EmptyBox>
-      <EmptyBox opacity={0.3} /> */}
     </Layout>
   )
 }
