996803ccd24b700b7edc656ef6a7225a5107d3b6
parkoon <devparkoon@gmail.com>
2022-04-05 23:21:35 +0900

    add address

--- src/components/common/Map/constants.ts
+++ src/components/common/Map/constants.ts
@@ -1,3 +1,5 @@
 export const lat = 37.53638106269606
 export const lng = 127.13226434095417
 export const destination = encodeURIComponent('루벨 강동점')
+export const address =
+  '서울특별시 강동구 \n 천호대로 1077 \n 이스트센트럴타워 35층'
