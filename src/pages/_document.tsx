25e3155310d61f03180538e0480b80f27454352c
parkoon <devparkoon@gmail.com>
2022-04-11 20:55:59 +0900

    update meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,27 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 function MyDocument() {
   return (
-    <Html>
+    <Html lang="ko-KR">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta charSet="utf-8" />
+        <meta name="robots" content="all" />
+        <meta
+          property="og:title"
+          content="🤵 박종혁 ♥️  👰🏻 김진아 결혼합니다"
+        />
+        <meta
+          property="og:description"
+          content="6월 18일 토요일 오후 12시 30분 강동 루벨"
+        />
+        <meta property="og:image" content="/images/wedding.png" />
+        <meta property="og:url" content="https://junebride.kr" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="ko_KR" />
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:url" content="https://junebride.kr" />
         {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
         <title>6월의 신부</title>
         <link
