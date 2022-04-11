6909e5f7b7824cf0272e5e74ae8dc162d58199b3
parkoon <devparkoon@gmail.com>
2022-04-11 22:28:16 +0900

    카카오링크 테스트중

--- src/hooks/useShareKakao.ts
+++ src/hooks/useShareKakao.ts
@@ -13,8 +13,25 @@ function useShareKakao() {
 
   const shareKakao = () => {
     window.Kakao.Link.sendScrap({
-      requestUrl: 'https://developers.kakao.com',
-      templateId: 74896,
+      objectType: 'feed', // 메시지 형식 : 피드 타입
+      content: {
+        title: '개성 넘치고 귀여운 인싸 친구, 아이비',
+        description: '나와 잘 맞는 식물 친구는?',
+        imageUrl: '/images/wedding.png', // 메인으로 보여질 이미지 주소
+        link: {
+          webUrl: '공유할 링크 주소',
+          mobileWebUrl: '공유할 링크 주소',
+        },
+      },
+      buttons: [
+        {
+          title: '나도 테스트 해보기', // 버튼 이름
+          link: {
+            webUrl: '공유할 링크 주소',
+            mobileWebUrl: '공유할 링크 주소',
+          },
+        },
+      ],
     })
   }
 
