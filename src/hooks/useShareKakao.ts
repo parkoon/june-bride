1d11396dd8f35514b173bcda1b480f85f038d504
parkoon <devparkoon@gmail.com>
2022-04-12 09:20:10 +0900

    test: custom template

--- src/hooks/useShareKakao.ts
+++ src/hooks/useShareKakao.ts
@@ -12,27 +12,34 @@ function useShareKakao() {
   }, [])
 
   const shareKakao = () => {
-    window.Kakao.Link.sendScrap({
-      objectType: 'feed', // 메시지 형식 : 피드 타입
-      content: {
-        title: '개성 넘치고 귀여운 인싸 친구, 아이비',
-        description: '나와 잘 맞는 식물 친구는?',
-        imageUrl: '/images/wedding.png', // 메인으로 보여질 이미지 주소
-        link: {
-          webUrl: '공유할 링크 주소',
-          mobileWebUrl: '공유할 링크 주소',
-        },
+    window.Kakao.Link.sendCustom({
+      templateId: 74896,
+      templateArgs: {
+        title: '제목 영역입니다.',
+        description: '설명 영역입니다.',
       },
-      buttons: [
-        {
-          title: '나도 테스트 해보기', // 버튼 이름
-          link: {
-            webUrl: '공유할 링크 주소',
-            mobileWebUrl: '공유할 링크 주소',
-          },
-        },
-      ],
     })
+    // window.Kakao.Link.sendDefault({
+    //     objectType: 'feed', // 메시지 형식 : 피드 타입
+    //     content: {
+    //       title: '개성 넘치고 귀여운 인싸 친구, 아이비',
+    //       description: '나와 잘 맞는 식물 친구는?',
+    //       imageUrl: '이미지 주소', // 메인으로 보여질 이미지 주소
+    //       link: {
+    //         webUrl: '공유할 링크 주소',
+    //         mobileWebUrl: '공유할 링크 주소',
+    //       },
+    //     },
+    //     buttons: [
+    //       {
+    //         title: '나도 테스트 해보기', // 버튼 이름
+    //         link: {
+    //           webUrl: '공유할 링크 주소',
+    //           mobileWebUrl: '공유할 링크 주소',
+    //         },
+    //       },
+    //     ],
+    //   })
   }
 
   return shareKakao
